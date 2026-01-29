6c0e2242d3c775dc8fb1e3e2a8e17d5924118440
Lucas Vinicius dos Santos <lucassantosvini17@gmail.com>
2026-01-29 17:23:11 -0300

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -5,7 +5,7 @@ insert into Autores (nome_autor) values ('Valter Bercelli');
 insert into Autores (nome_autor) values ('Sérgio Silva');
  
 SELECT * FROM Alunos; -- Comandos pra conmsultaer todos os valores da tabela Aluno 
-SELECT * From Autores;
+SELECT * FROM Autores;
 SELECT * FROM Editoras; -- Comandos pra consultar todos os valores da tabela Editora
 SELECT * FROM Livros; -- Comandos pra cosultar todos os valores da tabela Livros
 SELECT * FROM Emprestimos; -- Comandos pra consultar todos os valores da tabela Emprestimo 
@@ -31,3 +31,16 @@ FROM Livros left outer join Autores
 
 INSERT INTO Emprestimo_Itens (id_emprestimo, id_livro) VALUES (1,1)
 INSERT INTO Emprestimo_Itens (id_emprestimo, id_livro) VALUES (1,2)
+
+										SELECT Livros.id_livro, Livros.livro_titulo, Livros.isbn, Emprestimos.data_emprestimo,
+											   Emprestimos.id_emprestimo, Autores.nome_autor, Alunos.aluno_nome,
+
+										FROM Alunos left outer join Autores
+												 ON Alunos.aluno_nome = Autores.nome_autor
+												 left outer join Editoras
+	
+SELECT Emprestimos.id_emprestimo, Emprestimos.data_emprestimo, Alunos.aluno_nome
+
+FROM Emprestimos left outer join Alunos
+			ON Emprestimos.id_aluno = Alunos.id_aluno
+			left outer join 
