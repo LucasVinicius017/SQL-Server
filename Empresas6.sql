3cf55a2c0713f8896012e6b41f437d84f97d15d4
Lucas Vinicius dos Santos <lucassantosvini17@gmail.com>
2026-02-03 14:03:25 -0300

    Update Empresas6.sql
    Update em relação a melhorias do aprendizado do meu banco SQL

--- Empresas6.sql
+++ Empresas6.sql
@@ -42,7 +42,7 @@ CREATE TABLE Departamentos
 		departamento_nome VARCHAR(100) NOT NULL,
 		departamento_telefone VARCHAR(20) NOT NULL,
 		departamento_gerente INT NOT NULL,
-		departamento_cidade INT NOT NULL
+		departamento_cidade INT NOT NULL,
 		FOREIGN KEY (departamento_cidade) REFERENCES Localidades (localidade_codigo)
 );
 
@@ -66,28 +66,43 @@ CREATE TABLE Funcionarios
 ALTER TABLE Departamentos
 ADD FOREIGN KEY (departamento_gerente) REFERENCES Funcionarios(funcionario_codigo);
 
+
 INSERT INTO Paises (pais_nome, pais_codigoDDI) VALUES ('Brasil','55');
 INSERT INTO Paises (pais_nome, pais_codigoDDI) VALUES ('Canada','1');
-
+DELETE ;
+UPDATE ; 
 
 INSERT INTO Localidades (localidade_nome, localidade_cep, localidade_pais, localidade_rua, localidade_uf) 
 			VALUES ('Presidente Prundente','14940378','Brasil', 'R. Manoel Goulart','SP');
 INSERT INTO Localidades (localidade_nome, localidade_cep, localidade_pais, localidade_rua, localidade_uf) 
 			VALUES ('Toronto','A1A 1A1', 'Canadá', 'Yonge Street', 'ON');
-
+DELETE ;
+UPDATE ; 
 
 INSERT INTO Grade_Salarios (gradesalario_descricao_nivel, gradesalario_salariomaximo, gradesalario_salariominimo)
 			VALUES ('Auxilar administrativo', '3000.00', '1800.00');
 INSERT INTO Grade_Salarios (gradesalario_descricao_nivel, gradesalario_salariomaximo, gradesalario_salariominimo)
 			VALUES ('Gerente', '4000.00', '2000.00');
-
+DELETE ;
+UPDATE ; 
 
 INSERT INTO Cargos (cargo_descricao, cargo_salario) 
 VALUES ('realizando tarefas essenciais como atendimento ao público', '2022.00');
 INSERT INTO Cargos (cargo_descricao, cargo_salario) 
 VALUES (' responsável por planejar, organizar e liderar equipes', '4000,00');
+DELETE ;
+UPDATE ; 
 
 INSERT INTO Departamentos (departamento_cidade, departamento_gerente, departamento_nome, departamento_telefone)
 VALUES ('Presidente Prudente', 'Rafaella', 'Departamento Administrativo', '18959595959');
 INSERT INTO Departamentos (departamento_cidade, departamento_gerente, departamento_nome, departamento_telefone)
 VALUES ('Toronto', 'Fernando', 'Departamento Administrativo', '18958585858');
+DELETE ;
+UPDATE ; 
+
+INSERT INTO Funcionarios (funcionario_cargo, funcionario_comissao, funcionario_contratacao, funcionario_departamento, funcionario_email, funcionario_gerente, funcionario_nascimento, funcionario_nome)
+VALUES ('Auxiliar administrativo','300.00','2018/05/01','Administrativo','administrativo01@gmail.com','Alex','1999/05/20','Fulano1');
+INSERT INTO Funcionarios (funcionario_cargo, funcionario_comissao, funcionario_contratacao, funcionario_departamento, funcionario_email, funcionario_gerente, funcionario_nascimento, funcionario_nome)
+VALUES ('Auxiliar operacional','100.00','2013/05/01','Operacional','operacional01@gmail.com','Vinicius','1995/08/20','Fulano2');
+DELETE ;
+UPDATE ; 
